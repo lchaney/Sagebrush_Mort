7ffd41f8b9405833a2953c7a47b8b4ad698c28c2
lchaney <l2chaney@gmail.com>
2015-06-17 10:04:22 -0600

    source other .r files

--- Analysis/04_SM_do.R
+++ Analysis/04_SM_do.R
@@ -4,6 +4,19 @@
 # This script is used to PRINT output needed for the Chaney et al 2015 Sagebrush mortatlity paper
 #==============================================================================================#
 
+#set working directory -- change this to where you have files saved to
+	setwd("~/GitHub/Sagebrush_Mort/Analysis")
+
+#source to LOAD data
+	source("01_SM_load.R")
+
+#source to CLEAN data
+	source("02_SM_clean.R")
+
+#source to perform FUNCTION on data
+	source("03_SM_func.R")
+
+
 #3 garden survival
 
 	#print summary of survivorship
