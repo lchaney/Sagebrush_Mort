81eced470b2bcdde006051768c63e17fc378af33
lchaney <l2chaney@gmail.com>
2015-06-17 09:38:52 -0600

    created file, added header

---
+++ Analysis/01_SM_load.R
@@ -0,0 +1,7 @@
+#==============================================================================================#
+# Script created by Lindsay Chaney 2015 - lchaney@byu.edu
+# Script created in version R 3.1.3 
+# This script is used to LOAD data needed for the Chaney et al 2015 Sagebrush mortatlity paper
+#==============================================================================================#
+
+
---
+++ Analysis/04_SM_do.R
@@ -0,0 +1,5 @@
+#==============================================================================================#
+# Script created by Lindsay Chaney 2015 - lchaney@byu.edu
+# Script created in version R 3.1.3 
+# This script is used to PRINT output needed for the Chaney et al 2015 Sagebrush mortatlity paper
+#==============================================================================================#
