69f3970ae8735632d2a907e86eb557d0ebebd230
lchaney <l2chaney@gmail.com>
2015-06-17 21:30:54 -0600

    update 3 garden km plot using ggsurv

--- Analysis/03_SM_func.R
+++ Analysis/03_SM_func.R
@@ -9,20 +9,8 @@
 	#load needed packages
 	library(survival)
 	library(ggplot2)
+	library(GGally)
 	
-	#function to be able to source from GIT (https://gist.github.com/28e06a0ac1fc6d29af3b.git)
-	source_https <- function(url, ...) {
-	  # load package
-	  require(RCurl)
-	 
-	  # parse and evaluate each .R script
-	  sapply(c(url, ...), function(u) {
-	    eval(parse(text = getURL(u, followlocation = TRUE, cainfo = system.file("CurlSSL", "cacert.pem", package = "RCurl"))), envir = .GlobalEnv)
-	  })
-	}
- 
-	#load package from gitHub -- ggkm -- provides use of ggplot to create kaplain meyer plots
-	source_https("https://raw.githubusercontent.com/dmenne/dmisc2/master/R/ggkm.R")
 #==============================================================================================#
 
 
@@ -33,20 +21,22 @@
 
 
 	#fit cox ph model to use in kaplain meyer plots
-	fitcph_garden <- coxph(Surv(time, death)~strata(garden), data=surv3d)
-	fitcph_typeO <- coxph(Surv(time, death)~strata(type), data=sdat_O)
-	fitcph_typeM <- coxph(Surv(time, death)~strata(type), data=sdat_M)
-	fitcph_typeE <- coxph(Surv(time, death)~strata(type), data=sdat_E)
+	sfit_garden <- survfit(Surv(time, death)~strata(garden), data=surv3d)
+	sfit_typeO <- survfit(Surv(time, death)~strata(type), data=sdat_O)
+	sfit_typeM <- survfit(Surv(time, death)~strata(type), data=sdat_M)
+	sfit_typeE <- survfit(Surv(time, death)~strata(type), data=sdat_E)
 
 		#create survivorship plot for all three gardens
 				#all three gardens
-				gar3plot <- plot(survfit(fitcph_garden), col = 1, lty = 2:4, lwd=4)
-					gar3leg <- legend(1, 0.3, 
-					       legend = levels(surv3d$garden), 
-					       lty = 2:4, 
-					       col = 1,
-					       lwd=3,
-					       title = "Survivorship by garden")
+				gar3plot <- ggsurv(sfit_garden, surv.col = "black",
+								   cens.col = "black") +
+								   theme_minimal() +
+								   theme(axis.line = element_line(color = "black", size = .25))+
+								   scale_linetype_manual(name="Garden",
+								   		values = 2:4) +
+								   guides(color=FALSE)
+
+
 				#ephraim
 				ephplot <- plot(survfit(fitcph_typeE), col = 2:6, lty=2, lwd=4)
 					ephleg <- legend(1, 0.3, 
