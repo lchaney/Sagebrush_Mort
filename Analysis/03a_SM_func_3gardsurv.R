ac282e805fb10f045fd7fedf115dc3f8f1394485
lchaney <l2chaney@gmail.com>
2015-07-23 11:21:59 -0600

    formating

--- Analysis/03a_SM_func_3gardsurv.R
+++ Analysis/03a_SM_func_3gardsurv.R
@@ -11,16 +11,16 @@
 
 
 	#fit cox ph model to use in kaplain meyer plots
-	sfit_garden <- survfit(Surv(time, death)~strata(garden), data=surv3d)
-	sfit_typeE <- survfit(Surv(time, death)~strata(type), data=sdat_E)
-	sfit_typeM <- survfit(Surv(time, death)~strata(type), data=sdat_M)
-	sfit_typeO <- survfit(Surv(time, death)~strata(type), data=sdat_O)
+	sfit_garden <- survfit(Surv(time, death) ~ strata(garden), data=surv3d)
+	sfit_typeE <- survfit(Surv(time, death) ~ strata(type), data=sdat_E)
+	sfit_typeM <- survfit(Surv(time, death) ~ strata(type), data=sdat_M)
+	sfit_typeO <- survfit(Surv(time, death) ~ strata(type), data=sdat_O)
 
 		#create survivorship plot for all three gardens
 				#all three gardens
 				gar3plot <- ggsurv_m(sfit_garden, surv.col = "black",
-								   cens.col = "black", size.est=1) +
-								   scale_linetype_manual(name="Garden",
+								   cens.col = "black", size.est = 1) +
+								   scale_linetype_manual(name = "Garden",
 								   		values = 2:4) +
 								   guides(color = FALSE) +
 								   xlim(0, 60) + ylim(0, 1) +
@@ -32,7 +32,7 @@
 
 				#ephraim
 				ephplot <- ggsurv_m(sfit_typeE, lty.est = 2, plot.cens = FALSE, size.est = 1) +
-								   scale_color_manual(name="Ephraim",
+								   scale_color_manual(name = "Ephraim",
 								   		breaks = c("T4x", "T2x", "W4x", "V2x", "V4x"),
 								   		values = c(T4x = "#e31a1c",
 								   				   T2x = "#ff7f00",
@@ -48,7 +48,7 @@
 								   		 legend.background = element_rect(colour = "gray"))
 				#Majors
 				majplot <- ggsurv_m(sfit_typeM, lty.est = 3, plot.cens = FALSE, size.est = 1) +
-								   scale_color_manual(name="Majors",
+								   scale_color_manual(name = "Majors",
 								   		breaks = c("T4x", "T2x", "W4x", "V2x", "V4x"),
 								   		values = c(T4x = "#e31a1c",
 								   				   T2x = "#ff7f00",
@@ -64,7 +64,7 @@
 								   		 legend.background = element_rect(colour = "gray"))
 			#Orchard
 				orchplot <- ggsurv_m(sfit_typeO, lty.est = 4, plot.cens = FALSE, size.est = 1) +
-								   scale_color_manual(name="Orchards",
+								   scale_color_manual(name = "Orchards",
 								   		breaks = c("T4x", "T2x", "W4x", "V2x", "V4x"),
 								   		values = c(T4x = "#e31a1c",
 								   				   T2x = "#ff7f00",
@@ -90,18 +90,18 @@
 
 	
 	#create plot (to append to climate data on when deaths occur)
-		death2 <- ggplot(data=surv3dd, aes(x=date, y=1, color= garden)) + 
-			geom_jitter(position = position_jitter(width = .5), alpha=.4, na.rm=TRUE) +
+		death2 <- ggplot(data = surv3dd, aes(x = date, y = 1, color = garden)) + 
+			geom_jitter(position = position_jitter(width = .5), alpha = 0.4, na.rm = TRUE) +
 		    scale_x_date(limits = as.Date(c('2010-01-01','2015-05-08'))) +
 		    theme_minimal() +
 		    scale_y_continuous(breaks = 1) +
-			labs(x="Year", y="Mortality") +
+			labs(x = "Year", y = "Mortality") +
 			scale_colour_manual(values = c("darkorange","indianred1", "steelblue"), 
-								labels=c("Ephraim", "Majors Flat", "Orchard")) +
-			theme(legend.title=element_blank(), 
+								labels = c("Ephraim", "Majors Flat", "Orchard")) +
+			theme(legend.title = element_blank(), 
 				  plot.margin = unit(c(-2.7,0.5,0.5,0.5), "lines"), 
 				  #plot.margins #top, #right, #bottom, #left
-				  legend.position="none",
+				  legend.position = "none",
 		   		  axis.text.y = element_blank(),
 		  		  axis.ticks.y = element_blank(),
 		  		  axis.ticks.margin = unit(1.1, "lines")
