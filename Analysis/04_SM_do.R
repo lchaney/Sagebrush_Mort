2d6a27730f1c3116eba14a9a2540b108907ec540
lchaney <l2chaney@gmail.com>
2015-09-15 08:07:11 -0600

    removed printing output because it is now in markdown file

--- Analysis/04_SM_do.R
+++ Analysis/04_SM_do.R
@@ -1,10 +1,8 @@
 #==============================================================================================#
 # Script created by Lindsay Chaney 2015 - lchaney@byu.edu
 # Script created in version R 3.1.3 
-# This script is used to PRINT output needed for the Chaney et al 2015 Sagebrush mortatlity paper
+# This script is used to source all files needed for the Chaney et al 2015 Sagebrush mortatlity paper
 #==============================================================================================#
-
-#=== change source file location ===#
 
 #==============================================================================================#
 # Set working directory and source LOAD, CLEAN, and FUNCTION scripts
@@ -17,100 +15,5 @@
 
 	#source to perform FUNCTION on data
 		source('Analysis/03_SM_func.R')
-
-#==============================================================================================#
-
-
-
-
-#==============================================================================================#
-# Printing output for 3 garden survival
-	
-	#print 2 by 2 kaplain meyer plot for all 3 gardens
-	km22plot
-	
-			#or individual km plots
-			#gar3plot
-			#ephplot
-			#majplot
-			#orchplot
-	
-	#save above plot to figures folder
-	save_plot("Output/SF1.png", km22plot,
-          ncol = 2,
-          nrow = 2,
-          base_aspect_ratio = 2)
-
-	#print summary of survivorship
-	surv3summary
-	
-	#print sample sizes tables for all 3 gardens
-	surv3dsample
-
-#==============================================================================================#
-	
-#==============================================================================================#
-# Printing output for climate
-	
-	#print death and climate plot for all 3 gardens
-	plot(deathclim) 
-	
-		#or individual graphs
-		#death2
-		#clim_graph
-	
-	#save above plot to figures folder
-	save_plot("Output/climgraph.png", plot(deathclim), base_aspect_ratio = 2)
-
-	#print out minimum monthly minimum temperatures by garden
-	mintempsmonth
-#==============================================================================================#
-
-
-
-#==============================================================================================#
-#Ephraim survival data
-
-	#print survival regression (Table 2)
-	summary_ephsurvlogn
-	
-	#print kaplien meyer plot with survival regression curves (Figure 1)
-	ephsurvplot_lognorm
-	
-	#save above plot to figures folder
-	#fig1
-	
-	#Print log rank test for survival curves
-	svdatlrtest
-	
-	#print posthoc pairwise comparison table of chisquare log-rank test 
-	lrchisqtable
-	
-		#and associated p-values
-	  pval_lrchisqtagible
-		
-		#new bonferoni corrected p-value for significane
-		newbfp
-		
-	#median survival
-	medsurv
-		
-	#probability of survival at different months -- column survival gives you those times 
-	probsurv
-
-	#poulation level differences in survival, median survival and probability of survival
-	poplrtest
-	popmedsurv
-	popprobsurv
-		
-#Ephraim climate survival data
-	#generalized linear model
-	climsurv
-	
-	#rsquared value
-	climsurvrsq
-	
-	#model coefficients
-	coefclimsurv
 
 #==============================================================================================#
