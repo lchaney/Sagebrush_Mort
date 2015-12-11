cfbaf02732d9fc07190647689a74a9ea1c3d719e
lchaney <l2chaney@gmail.com>
2015-12-11 10:28:20 -0700

    changed how to save plots - saving deathclim is currently unsupported

--- Analysis/04_SM_do.R
+++ Analysis/04_SM_do.R
@@ -28,7 +28,7 @@
 		
 		#Figure 1
 		#Kaplien-Meir survivorship curves by type (subspecies:ploidy) in big sagebrush grown in Eprhaim
-		  save_plot("Output/Fig1.png", ephsurvplot_lognorm)
+		  ggsave("Output/Fig1.png", ephsurvplot_lognorm)
 		
 		#Supplementary Figure 2
 		#Kaplien-Meir survivorship curves for big sagebrush by (a) common garden and survivor ship by type (subspecies:ploidy) for the (b) Ephraim, (c) Majors Flat and (d) Orchards garden.
@@ -39,10 +39,16 @@
 		
 		#Supplementary Figure 3
 		#Interaction plot of genetics and environment for each population.
-		  save_plot("Output/SuplFig3.png", int_plot)
+		  ggsave("Output/SuplFig3.png", int_plot)
 		
 		#Supplementary Figure 4
 		#Top panel shows minimum daily temperatures (light lines) with 30 day moving averages (bold lines) for the three garden locations during the experimental time period. 
-		  save_plot("Output/SuplFig4.png", deathclim, base_aspect_ratio = 2)
+		  #this is not currently supported, but should be soon.
+		  #see https://github.com/baptiste/gridextra/issues/13
+		  #A hack to get around this with ggplot2 v1.0.1, but I will leave this commented out for now
+		  #ggsave <- ggplot2::ggsave; body(ggsave) <- body(ggplot2::ggsave)[-2];
+		  
+		  #save_plot("Output/SuplFig4.png", deathclim, base_aspect_ratio = 2)
+		  
 
 #==============================================================================================#	
